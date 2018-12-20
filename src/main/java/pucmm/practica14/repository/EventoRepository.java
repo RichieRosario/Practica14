18a2ff5e6b75de22c55cdbef855b812b85bd7580
Shantall Giron <shantgiron@gmail.com>
2018-12-20 11:21:31 -0400

    Update EventoRepository.java

--- src/main/java/pucmm/practica14/repository/EventoRepository.java
+++ src/main/java/pucmm/practica14/repository/EventoRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import pucmm.practica14.model.Evento;
 
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 public interface EventoRepository extends JpaRepository<Evento, Long> {
@@ -13,5 +14,10 @@ public interface EventoRepository extends JpaRepository<Evento, Long> {
 
     @Query(value = "SELECT * FROM evento m offset(?1) limit(?2)", nativeQuery = true)
     List<Evento> eventosPaginados(int offset, int limit);
+
+     //otra manera
+
+    @Query("select evento from Evento evento where evento.fecha between ?1 and ?2")
+    List<Evento> eventosPorFecha(Date fechaInicio, Date fechaFin);
 
 }
